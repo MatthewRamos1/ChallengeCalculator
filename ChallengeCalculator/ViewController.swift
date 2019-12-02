1d73d42e13c5b1dbe76b0dbfb3ed97f383d20d00
Matthew Ramos <mmramos309@gmail.com>
2019-12-02 12:46:30 -0500

    Set up and constrained storyboard

--- ChallengeCalculator/ViewController.swift
+++ ChallengeCalculator/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum ChallengeType {
+        case distance
+        case elevation
+    }
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var metricTextField: UITextField!
+    @IBOutlet weak var numberOfWeeksTextField: UITextField!
+    
+    var currentChallenge: ChallengeType = ChallengeType.elevation {
+        didSet {
+            calculateChallenge()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func calculateChallengeButton(_ sender: UIButton) {
+    }
+    
+    func calculateChallenge() {
+        guard let metric = Double(metricTextField.text ?? ""), let numberOfWeeks = Int(numberOfWeeksTextField.text ?? "") else {
+            print("missing required fields")
+            return }
+    }
 
 }
 
